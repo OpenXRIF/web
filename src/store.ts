9c70c1a7c00da51d5c1f03c1f03d80c570e8aa32
Fraser Morrison <frazemor@hotmail.com>
2025-03-18 16:08:10 -0400

    Made activities available to store + logging

--- src/store.ts
+++ src/store.ts
@@ -1,46 +1,127 @@
 import { create } from "zustand";
 import { fetchXRIF } from "./endpoints";
 
+type ActionNavigate = {
+  action: "navigate";
+  input: {
+    name: string;
+    x: number;
+    y: number;
+  };
+};
+
+type ActionSpeak = {
+  action: "speak";
+  input: string;
+};
+
+type ActionWait = {
+  action: "wait";
+  input: number;
+};
+
+type Action = ActionNavigate | ActionSpeak | ActionWait;
+
+type XRIF = Record<string, any> & {
+  actions: Action[];
+};
+
 type StoreState = {
   xrifPromise: Promise<any> | null;
+  xrifValue: XRIF | null;
   xrifKey: string;
   highlightedAction: number;
-  log: string[];
+  speechBubble: string | null;
+  logArray: string[];
 };
 
 interface StoreFunctions {
   getXrif: (message: string) => void;
   highlightAction: (idx: number) => void;
   executeAction: () => void;
+  log: (message: string) => void;
 }
 
 const defaultState: StoreState = {
   xrifPromise: null,
+  xrifValue: null,
   xrifKey: "actions.0",
   highlightedAction: 0,
-  log: [],
+  speechBubble: null,
+  logArray: [],
 };
 
 export const useXrifStore = create<StoreState & StoreFunctions>((set) => ({
   ...defaultState,
+  log: (message: string) => {
+    set((state) => {
+      state.logArray.push(message);
+      console.log(message);
+      return {
+        logArray: state.logArray,
+      };
+    });
+  },
   getXrif: (message: string) => {
     const response = fetchXRIF(message);
-    set({ xrifPromise: response });
+    set((state) => {
+      state.log(`Fetched XRIF for message: ${message}`);
+      return {
+        xrifPromise: response,
+        highlightedAction: 0,
+        xrifKey: "actions.0",
+      };
+    });
+    response.then((xrif) => {
+      set({ xrifValue: xrif });
+    });
   },
   highlightAction: (idx: number) => {
-    set({ xrifKey: `actions.${idx}`, highlightedAction: idx });
+    set({
+      xrifKey: `actions.${idx}`,
+      highlightedAction: idx,
+    });
   },
   executeAction: () => {
     set((state) => {
-      // Execute the action here.
+      // Clear robot speach bubble.
+      state.speechBubble = null;
 
-      state.log.push(`Executed action ${state.highlightedAction}`);
-      console.log(`Executed action ${state.highlightedAction}`);
+      // Reset if past length
+      if (state.highlightedAction >= state.xrifValue?.actions.length) {
+        // TODO: Set finished condition
+        state.log("Finished executing XRIF.");
+        return {};
+      }
+
+      const action = state.xrifValue?.actions[state.highlightedAction];
+      if (!action) {
+        state.log(`No action found for index ${state.highlightedAction}`);
+        return {};
+      }
+
+      // TODO: Execute the action here.
+      if (action.action === "speak") {
+        state.log(`Speaking: ${action.input}`);
+        state.speechBubble = action.input;
+      }
+
+      if (action.action === "navigate") {
+        // TODO
+        state.log(`Navigating to ${action.input.name}`);
+      }
+
+      if (action.action === "wait") {
+        // TODO
+        state.log(`Waiting for ${action.input} seconds`);
+      }
+
+      state.log(`Executed action ${state.highlightedAction}`);
 
       return {
         highlightedAction: state.highlightedAction + 1,
         xrifKey: `actions.${state.highlightedAction + 1}`,
-        log: state.log,
+        logArray: state.logArray,
       };
     });
   },
